3fb93719c77949ad13cb78caf6be7abf072f208a
Jason Cardinal <93737375+Metaluv@users.noreply.github.com>
2022-12-30 20:48:37 -0700

    update

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -44,7 +44,7 @@ export default function About() {
         <title>About - Jason Cardinal</title>
         <meta
           name="description"
-          content="I’m Jason Cardinal. I live in Meadow Lake, Saskatchewan, where I design the future."
+          content="I’m Jason Cardinal. I live in Saskatchewan, where I design the future."
         />
       </Head>
       <Container className="mt-16 sm:mt-32">
@@ -61,35 +61,21 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-              I’m Jason Cardinal. I live in Meadow Lake, Saskatchewan, where I design the
+              I’m Jason Cardinal. I live in Saskatchewan, where I design the
               future.
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I believe that people are at the heart of everythign we do. I am dedicated to empowering people by listening to them and understanding their needs, then providing them with the tools they need to succeed.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                I have a background in finance and technology, and I am currently working on a project to help people learn about the stock market. I recently graduated from the University of Saskatchewan with a Bachelor of Science degree, and I am currently working on Automation & Digital Agriculture.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
+                I spent enjoy spending time with my family, playing video games, and learning about new technologies. I also enjoy wakeboarding, snowboarding, and playing the guitar. I also have a passion for robotics, artificial intelligence, and I am drone pilot with a sRPAS VLOS certificate.
               </p>
               <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                Today, I’m the Community Navigator for Flying Dust First Nation and the founder of Cardinal Trading Co. & Emberstone Fusion Kitchen, where we are working on finding ways to help people enter the e-commerce space.
               </p>
             </div>
           </div>
