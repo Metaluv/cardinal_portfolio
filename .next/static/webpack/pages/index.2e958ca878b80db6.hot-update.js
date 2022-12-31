2a762c90f25d6ab99b49734afbe2b3614805b9a5
Jason Cardinal <93737375+Metaluv@users.noreply.github.com>
2022-12-30 20:25:03 -0700

    update

---
+++ .next/static/webpack/pages/index.2e958ca878b80db6.hot-update.js
@@ -0,0 +1,42 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/index",{
+
+/***/ "./src/images/photos/image-1.jpg":
+/*!***************************************!*\
+  !*** ./src/images/photos/image-1.jpg ***!
+  \***************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/image-1.7d7e1976.jpg\",\"height\":1563,\"width\":1563,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-1.7d7e1976.jpg&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":8});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW1hZ2VzL3Bob3Rvcy9pbWFnZS0xLmpwZy5qcyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsQ0FBQyxvTUFBb00iLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vc3JjL2ltYWdlcy9waG90b3MvaW1hZ2UtMS5qcGc/NmFjOCJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XCJzcmNcIjpcIi9fbmV4dC9zdGF0aWMvbWVkaWEvaW1hZ2UtMS43ZDdlMTk3Ni5qcGdcIixcImhlaWdodFwiOjE1NjMsXCJ3aWR0aFwiOjE1NjMsXCJibHVyRGF0YVVSTFwiOlwiL19uZXh0L2ltYWdlP3VybD0lMkZfbmV4dCUyRnN0YXRpYyUyRm1lZGlhJTJGaW1hZ2UtMS43ZDdlMTk3Ni5qcGcmdz04JnE9NzBcIixcImJsdXJXaWR0aFwiOjgsXCJibHVySGVpZ2h0XCI6OH07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/images/photos/image-1.jpg\n"));
+
+/***/ }),
+
+/***/ "./src/images/photos/image-4.jpg":
+/*!***************************************!*\
+  !*** ./src/images/photos/image-4.jpg ***!
+  \***************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/image-4.b1dfac48.jpg\",\"height\":1563,\"width\":1563,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-4.b1dfac48.jpg&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":8});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW1hZ2VzL3Bob3Rvcy9pbWFnZS00LmpwZy5qcyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsQ0FBQyxvTUFBb00iLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vc3JjL2ltYWdlcy9waG90b3MvaW1hZ2UtNC5qcGc/YzU5MCJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XCJzcmNcIjpcIi9fbmV4dC9zdGF0aWMvbWVkaWEvaW1hZ2UtNC5iMWRmYWM0OC5qcGdcIixcImhlaWdodFwiOjE1NjMsXCJ3aWR0aFwiOjE1NjMsXCJibHVyRGF0YVVSTFwiOlwiL19uZXh0L2ltYWdlP3VybD0lMkZfbmV4dCUyRnN0YXRpYyUyRm1lZGlhJTJGaW1hZ2UtNC5iMWRmYWM0OC5qcGcmdz04JnE9NzBcIixcImJsdXJXaWR0aFwiOjgsXCJibHVySGVpZ2h0XCI6OH07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/images/photos/image-4.jpg\n"));
+
+/***/ }),
+
+/***/ "./src/images/photos/image-5.jpg":
+/*!***************************************!*\
+  !*** ./src/images/photos/image-5.jpg ***!
+  \***************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/image-5.5f46c761.jpg\",\"height\":1563,\"width\":1563,\"blurDataURL\":\"/_next/image?url=%2F_next%2Fstatic%2Fmedia%2Fimage-5.5f46c761.jpg&w=8&q=70\",\"blurWidth\":8,\"blurHeight\":8});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvaW1hZ2VzL3Bob3Rvcy9pbWFnZS01LmpwZy5qcyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsQ0FBQyxvTUFBb00iLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vc3JjL2ltYWdlcy9waG90b3MvaW1hZ2UtNS5qcGc/ZWU2MyJdLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCB7XCJzcmNcIjpcIi9fbmV4dC9zdGF0aWMvbWVkaWEvaW1hZ2UtNS41ZjQ2Yzc2MS5qcGdcIixcImhlaWdodFwiOjE1NjMsXCJ3aWR0aFwiOjE1NjMsXCJibHVyRGF0YVVSTFwiOlwiL19uZXh0L2ltYWdlP3VybD0lMkZfbmV4dCUyRnN0YXRpYyUyRm1lZGlhJTJGaW1hZ2UtNS41ZjQ2Yzc2MS5qcGcmdz04JnE9NzBcIixcImJsdXJXaWR0aFwiOjgsXCJibHVySGVpZ2h0XCI6OH07Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/images/photos/image-5.jpg\n"));
+
+/***/ })
+
+});
